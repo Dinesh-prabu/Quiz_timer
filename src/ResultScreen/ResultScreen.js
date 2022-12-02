d3872b8b4caea9410f9127979162e06f754fb6c9
Dinesh-prabu <60753467+Dinesh-prabu@users.noreply.github.com>
2022-12-02 08:03:39 +0530

    Initial commit

---
+++ src/ResultScreen/ResultScreen.js
@@ -0,0 +1,29 @@
+import react from "react";
+import "../styles.css";
+
+import { PieChart } from "react-minimal-pie-chart";
+
+const ResultScreen = ({ score, total }) => {
+  return (
+    <div>
+      {`Score :  ${score}`}
+      <div className="piechart">
+        <PieChart
+          data={[
+            {
+              title: "Correct",
+              value: score,
+              color: "green",
+              label: "teee",
+              reveal: "50%"
+            },
+            { title: "Wrong", value: total - score, color: "red" },
+            { title: "Total", value: total, color: "blue" }
+          ]}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ResultScreen;
